3bb4b47c18e8929fc59565980b6122d9a476a2b2
Momasz <46492374+TomaszH123@users.noreply.github.com>
2023-09-03 19:49:47 +0200

    Update main.c

--- main.c
+++ main.c
@@ -8,8 +8,10 @@
 
 int check_number(_Dcomplex imag);
 int convert_coords(int value);
-int convert_coords_x(double x);
-int convert_coords_y(double y);
+int convert_coords_x(double x, int* pscale_factor);
+int convert_coords_y(double y, int* pscale_factor);
+void draw_fractal(SDL_Renderer* renderer, int* pscale_factor);
+void change_scale(SDL_Renderer* renderer, SDL_Event* event, int* pscale_factor);
 
 
 int main() {
@@ -18,6 +20,11 @@ int main() {
 	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 	int running = 1;
 	SDL_Event event;
+
+	int scale_factor = 1;
+	int* pscale_factor = &scale_factor;
+
+	int drawn = 0;
 
 
 
@@ -28,28 +35,15 @@ int main() {
 			if (event.type == SDL_QUIT) {
 				running = 0;
 			}
+
+			change_scale(renderer, &event, pscale_factor);
 		}
-		
-		
-		double a = -2;
-		double b = 2;
-		_Dcomplex z = { a, b };
-		for (int i = 0; i < 800; i++) {
-			for (int j = 0; j < 800; j++) {
-				if (check_number(z)) {
-					SDL_RenderDrawPoint(renderer, convert_coords_x(a), convert_coords_y(b));
-					//printf("%d  %d\n", convert_coords_x(a), convert_coords_y(b));
-					//SDL_RenderPresent(renderer);
-				}
-				a = a + 0.005;
-				z._Val[0] = a;
-				z._Val[1] = b;
-			}
-			b = b - 0.005;
-			a = -2; // Reset 'a' for the next row
-			z._Val[0] = a;
-			z._Val[1] = b;
+
+		if (!drawn) {
+			draw_fractal(renderer, pscale_factor);
+			drawn = 1;
 		}
+
 
 		//_Dcomplex z = { 1, 0 };
 		//printf("%d\n", check_number(z));
@@ -85,12 +79,46 @@ int convert_coords(int value) {
 	return x_screen;
 }
 
-int convert_coords_x(double x) {
+int convert_coords_x(double x, int* pscale_factor) {
 	int a = (x + 2) * 200;
 	return a;
 }
 
-int convert_coords_y(double y) {
+int convert_coords_y(double y, int* pscale_factor) {
 	int a = 800 - ((y + 2) * 200);
 	return a;
+}
+
+void draw_fractal(SDL_Renderer* renderer, int* pscale_factor) {
+	double a = -2;
+	double b = 2;
+	_Dcomplex z = { a, b };
+	for (int i = 0; i < 800; i++) {
+		for (int j = 0; j < 800; j++) {
+			if (a < 0.5 && b < 1 && b > -1){
+				if (check_number(z)) {
+					SDL_RenderDrawPoint(renderer, convert_coords_x(a, pscale_factor), convert_coords_y(b, pscale_factor));
+					//printf("%d  %d\n", convert_coords_x(a), convert_coords_y(b));
+					//SDL_RenderPresent(renderer);
+				}
+			}
+			
+			a = a + 0.005;
+			z._Val[0] = a;
+			z._Val[1] = b;
+		}
+		b = b - 0.005;
+		a = -2; // Reset 'a' for the next row
+		z._Val[0] = a;
+		z._Val[1] = b;
+	}
+}
+
+void change_scale(SDL_Renderer* renderer, SDL_Event* event, int* pscale_factor) {
+	if (event->type == SDL_MOUSEWHEEL) {
+		if (event->wheel.y > 0) {
+			printf("Scrolled up!\n");
+			*pscale_factor = (*pscale_factor) * 2;
+		}
+	}
 }
